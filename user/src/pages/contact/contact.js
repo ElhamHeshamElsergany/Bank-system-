9cca5878e8ca6aeb3379e39ebb0df017ffab358d
ElhamHeshamElsergany <elhamhesham18@gmail.com>
2022-03-30 14:24:46 +0200

    add routing and some features

---
+++ user/src/pages/contact/contact.js
@@ -0,0 +1,13 @@
+import Navbar from '../../components/navbar/navbar';
+import './contact.scss';
+
+function Contact() {
+    return (
+        <>
+        <Navbar/>
+            <h1>contact</h1>
+        </>
+    );
+}
+
+export default Contact;
---
+++ user/src/pages/transforming/transforming.js
@@ -0,0 +1,14 @@
+
+import Navbar from '../../components/navbar/navbar';
+import './transforming.scss';
+
+function Transformaing() {
+    return (
+        <>
+        <Navbar/>
+            <h1>transforming</h1>
+        </>
+    );
+}
+
+export default Transformaing;
